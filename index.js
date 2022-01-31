61cefce492729fcc55f1fd3b2ceb076a3ed20851
wakenedo <a_nuner@outlook.com>
2022-01-31 14:34:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('http');
-
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,10 +11,25 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', (socket) => {
+app.use(cors());
+app.use(router);
+
+io.on('connect', (socket) => {
     console.log('We have a new connection!!!');
 
-    socket.on('disconect', () => {
+    socket.on('join', ({name, room}, callback) => {
+        console.log(name,room)
+
+        const err = true;
+
+        if(err) {
+            callback({error: 'error'});
+
+        }
+
+    })
+
+    socket.on('disconnect', () => {
         console.log('User had left !!!')
     })
 });
