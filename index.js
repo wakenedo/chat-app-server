0a5ee796f7205e23d6a5f9edc748fe03b1bcd1d8
wakenedo <a_nuner@outlook.com>
2022-02-04 14:19:27 -0300

    router & index - Quick Update

--- index.js
+++ index.js
@@ -10,28 +10,27 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.use(cors());
+
+
 app.use(router);
 
 io.on('connect', (socket) => {
     console.log('We have a new connection!!!');
-
-    socket.on('join', ({name, room}, callback) => {
-        console.log(name,room)
+    socket.on('join', ({ name, room }, callback) => {
+        console.log(name, room)
 
         const err = true;
 
-        if(err) {
-            callback({error: 'error'});
+        if (err) {
+            callback({ error: 'error' });
 
         }
-
+        callback();
     })
 
     socket.on('disconnect', () => {
         console.log('User had left !!!')
     })
 });
-
-app.use(router)
 
 server.listen(process.env.PORT || 5000, () => console.log(`Server has started`));
